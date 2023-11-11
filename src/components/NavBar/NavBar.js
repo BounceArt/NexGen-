d44875bd1869b023b2a4820dafb228ea1879be8a
Alejo Aranguren <alejoaranguren2005@gmail.com>
2023-11-11 19:10:17 -0300

    se agrega el proyecto

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,10 @@
+ export const NavBar = () =>{
+    return <div className="navbar">
+        <ul className="lista">
+            <li><a className="link"href="">Home</a></li>
+            <li><a href="">Productos</a></li>
+            <li><a href="">Contacto</a></li>
+            <li><a href="">Carrito</a></li>
+        </ul>
+    </div>
+}
