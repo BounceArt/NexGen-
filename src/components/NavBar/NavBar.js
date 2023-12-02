755e58512d4ddc77a6f1dc8315d7b71a06edff44
Alejo Aranguren <alejoaranguren2005@gmail.com>
2023-12-02 14:44:09 -0300

    json de productos

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,14 @@
 import "./NavBar.css"  
+import M from 'materialize-css';
+import React, { useEffect } from 'react'
 import {Carrito, Numerito} from "../CartWidget/CartWidget"
  export const NavBar = () =>{
+  useEffect(() => {
+      const elems = document.querySelectorAll('.dropdown-trigger')
+       M.Dropdown.init(elems, { hover: false })
+    })
+  
+
     return <div className="container">
     <nav className="nav-extended">
         <div className="nav-wrapper">
@@ -11,15 +19,16 @@ import {Carrito, Numerito} from "../CartWidget/CartWidget"
             <li><a className="link" href="">Home</a></li>
             <li><a className="link" href="">Sobre Nosotros</a></li>
             
-            <li><a className="dropdown-trigger link" href="#!" data-target="dropdown1">Productos<i className="material-icons right">arrow_drop_down</i></a></li>
+            <li><a className="dropdown-trigger links" href="#!" data-target="dropdown1">Productos<i className="material-icons right">arrow_drop_down</i></a></li>
             <li className="carrito"><a className="link"><Carrito/>Carrito<Numerito/></a> </li>
           </ul>
         </div>
         <ul id="dropdown1" className="dropdown-content">
-          <li><a className="link" href="#!">Todos los productos</a></li>
+          <li><a className="link" href="#!">Auriculares</a></li>
+          <li className="divider link"></li>
           <li><a className="link" href="#!">Celulares</a></li>
           <li className="divider link"></li>
-          <li><a className="link" href="#!"></a></li>
+          <li><a className="link" href="#!">Monitores</a></li>
         </ul>
     </nav>
       </div>
